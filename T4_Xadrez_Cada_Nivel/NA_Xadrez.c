106ed95ef1f732d3f3a01db09bb39779ce70a397
Vinicius Silva <243342205+vininjanc@users.noreply.github.com>
2025-12-27 17:09:50 -0300

    Pseudocódigo ajustado na entrada sobre o cavalo.

--- T4_Xadrez_Cada_Nivel/NA_Xadrez.c
+++ T4_Xadrez_Cada_Nivel/NA_Xadrez.c
@@ -8,6 +8,7 @@
             Torre: Move 5 casas para direita
             Bispo: Move 5 casas para a diagonal superior direita (cima direita x 5)
             Rainha: Move 8 casas para a esquerda
+            Cavalo: Move duas casas para baixo e uma para direita (NA).
 
 
 2. Processamento
